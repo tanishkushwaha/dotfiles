4f9c2ab6f7110f12116d2f8298cd2d33d69a0259
Tanish <55587312+tanishkushwaha@users.noreply.github.com>
2021-09-30 12:01:01 +0530

    Delete 1/.cache directory

--- 1/.cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#ceaca6", "#0c0b19" },
-	[SchemeSel] = { "#ceaca6", "#4E3355" },
-	[SchemeOut] = { "#ceaca6", "#994B4C" },
-};

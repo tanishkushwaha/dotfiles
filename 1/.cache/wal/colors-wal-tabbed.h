ed9f459e7708d4be755769fca5f344311f6e263f
Tanish <55587312+tanishkushwaha@users.noreply.github.com>
2021-09-30 12:11:31 +0530

    Delete 1/.cache/wal directory

--- 1/.cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#0c0b19";
-static const char* selfgcolor   = "#ceaca6";
-static const char* normbgcolor  = "#6B3B54";
-static const char* normfgcolor  = "#ceaca6";
-static const char* urgbgcolor   = "#4E3355";
-static const char* urgfgcolor   = "#ceaca6";
